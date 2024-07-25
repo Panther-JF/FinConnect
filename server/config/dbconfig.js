2955c221861bef531b3034415c9366f2746807d3
Panther-JF <itsjayesh.tech@gmail.com>
2024-07-26 00:43:16 +0530

    Resolve Error

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose');
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        console.log(`\n MongoDB connected `);
 
     }
     catch (error) {
